59cfa9b0e5083ff765dcd771c45423c30e436f45
Anthony Roux <anthony.rx43@gmail.com>
2020-10-02 18:11:48 +0200

    Adjust timestamp to UTC before writing

--- src/main/java/io/github/anthorx/parquet/sql/write/converter/types/TimestampConverter.java
+++ src/main/java/io/github/anthorx/parquet/sql/write/converter/types/TimestampConverter.java
@@ -1,35 +1,48 @@
 package io.github.anthorx.parquet.sql.write.converter.types;
 
-import io.github.anthorx.parquet.sql.write.converter.ConvertException;
 import io.github.anthorx.parquet.sql.model.SQLColumnDefinition;
 import io.github.anthorx.parquet.sql.model.SQLField;
 import io.github.anthorx.parquet.sql.record.RecordField;
+import io.github.anthorx.parquet.sql.write.converter.ConvertException;
 import io.github.anthorx.parquet.sql.write.converter.PrimitiveTypeCreator;
 import org.apache.parquet.io.api.RecordConsumer;
 import org.apache.parquet.schema.LogicalTypeAnnotation;
 import org.apache.parquet.schema.PrimitiveType;
 
 import java.sql.Timestamp;
+import java.util.TimeZone;
 
 public class TimestampConverter implements ParquetSQLConverter {
 
-    @Override
-    public boolean accept(Class<?> c) {
-        return c.isAssignableFrom(Timestamp.class);
-    }
+  @Override
+  public boolean accept(Class<?> c) {
+    return c.isAssignableFrom(Timestamp.class);
+  }
 
-    @Override
-    public RecordField<?> convert(SQLField sqlField) throws ConvertException {
-        Timestamp timestamp = (Timestamp) sqlField.getValue();
-        return new RecordField<>(sqlField.getName(), timestamp.getTime())
-            .addWriteConsumer(RecordConsumer::addLong);
-    }
+  /**
+   * Convert a sqlField to A RecordField.
+   * Add offset from UTC because JDBC timestamp is dependant to the local timezone
+   *
+   * @param sqlField
+   * @return
+   * @throws ConvertException
+   */
+  @Override
+  public RecordField<?> convert(SQLField sqlField) {
+    Timestamp timestamp = (Timestamp) sqlField.getValue();
+    long timestampMs = timestamp.getTime();
+    long offset = TimeZone.getDefault().getOffset(timestampMs);
+    long timestampUtcAdjusted = timestampMs + offset;
 
-    @Override
-    public PrimitiveType convert(SQLColumnDefinition sqlColumnDefinition) throws ConvertException {
-        return PrimitiveTypeCreator.create(sqlColumnDefinition.getName(),
-                PrimitiveType.PrimitiveTypeName.INT64,
-                LogicalTypeAnnotation.timestampType(true, LogicalTypeAnnotation.TimeUnit.MILLIS),
-                sqlColumnDefinition.isNullable());
-    }
+    return new RecordField<>(sqlField.getName(), timestampUtcAdjusted)
+        .addWriteConsumer(RecordConsumer::addLong);
+  }
+
+  @Override
+  public PrimitiveType convert(SQLColumnDefinition sqlColumnDefinition) {
+    return PrimitiveTypeCreator.create(sqlColumnDefinition.getName(),
+        PrimitiveType.PrimitiveTypeName.INT64,
+        LogicalTypeAnnotation.timestampType(true, LogicalTypeAnnotation.TimeUnit.MILLIS),
+        sqlColumnDefinition.isNullable());
+  }
 }
